3fdd205251b07cadad1be7585db6056e8a03da4f
DinNamprom <56390732+DinNamprom@users.noreply.github.com>
2024-12-27 15:51:53 +0700

    lab13_2 complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,29 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool x[][M]) {
+    cout << "------------------------------------------------------------------------";
+    for (int j=0;j<N;j++){
+        cout << "\n|";
+        for (int i=0;i<M;i++){
+            if (x[j][i] == 1) {
+                cout << "*";
+            }else {
+                cout << " ";
+            }
+        }
+        cout << "|";
+    }
+    cout << "\n------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool X[][M],int s,int x,int y) {
+    for (int i=0;i<M;i++) {
+        for(int j=0;j<N;j++) {
+            if (sqrt((pow((j-x),2)+pow((i-y),2))) <= s-1) {
+                X[j][i] = 1;
+            }
+        }
+    }
+}
